500bee5b13dcb7917e298b0c9e1a93231f1b6789
Bradley Ezard <bradley.ezard@postgrad.curtin.edu.au>
2021-10-07 13:31:36 +0800

    Added swin model

---
+++ swin/__init__.py
@@ -0,0 +1,2 @@
+import swin.layers
+from swin.swin import SwinBlock, build_swin_model
--- swin/swin.py
+++ swin/swin.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 import torch
 from torch import nn
 
@@ -50,3 +51,44 @@ class SwinBlock(nn.Module):
         result = self.final_activation(residual + features)
         
         return result
+
+
+def build_swin_model(
+    out_classes: int,
+    layer_configuration: Sequence[int] = [2, 2, 6, 2],
+    initial_channels: int = 96,
+    patch_size: int = 4,
+    heads: int = 3,
+    window_size: int = 4,
+    offset_size: int = 2,
+    expansion: int = 4,
+    dropout: float = 0.1,
+) -> nn.Sequential:
+    modules = [layers.PatchPartition(patch_size),
+               nn.Conv2d(patch_size * patch_size * 3, initial_channels, 1)]  # Linear patch embedding
+    
+    channels = initial_channels
+    shift = False
+    for config in layer_configuration:
+        # Add the Swin Blocks
+        for _ in range(config):
+            modules.append(SwinBlock(
+                channels,
+                heads,
+                window_size,
+                shift_offset=offset_size if shift else 0,
+                expansion=expansion,
+                dropout=dropout,
+            ))
+            shift = not shift
+        
+        # Add the PatchMerge
+        modules.append(layers.PatchMerge(channels))
+        channels = channels * 2
+    
+    return nn.Sequential(
+        *modules,
+        nn.AdaptiveAvgPool2d(1),  # Global Average Pooling
+        nn.Flatten(1),
+        nn.Linear(channels, out_classes)
+    )
